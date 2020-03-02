3a7798e6c76a8821838f6d18a564d6b765e58ae7
tutorials <tutorials@Francos-MacBook-Pro.local>
2020-03-01 21:46:24 -0300

    Navbar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar, Nav, Button } from 'react-bootstrap';
+const Header = () => {
+    return (
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand href="#home">Skin and Bone</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Adopt</Nav.Link>
+                <Nav.Link href="#features">Lost</Nav.Link>
+                <Nav.Link href="#pricing">Found</Nav.Link>
+                <Nav.Link href="#pricing">Add new animal</Nav.Link>
+                <Nav.Link href="#pricing">My animals</Nav.Link>
+            </Nav>
+            <Button>Login / Sign up</Button>
+        </Navbar>
+    )
+}
+
+export default Header
