c16ecba2ee2cc0628672f8124ef106efb209e27e
tutorials <tutorials@Francos-MacBook-Pro.local>
2020-05-04 21:08:02 -0300

    styling add new animal

--- src/components/NewAnimal/index.tsx
+++ src/components/NewAnimal/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Container, WrapperInfo, Title, SubMenu, Label, Input, ContainerInputs, Button } from './styled'
+import { Container, Wrapper, Title, Label, Input, ContainerInputs, Button, ContainerInput } from './styled'
 
 export const NewAnimal = () => {
     const [city, setCity] = useState<string>('')
@@ -22,7 +22,7 @@ export const NewAnimal = () => {
         let animalBase64 = '';
         getBase64(fileUploaded, (result: any) => {
             animalBase64 = result;
-            console.log(animalBase64)
+
         });
     }
 
@@ -33,24 +33,29 @@ export const NewAnimal = () => {
         //picture
         <Container>
             <Title>Add a New Animal</Title>
-            <WrapperInfo>
-                <SubMenu>Info</SubMenu>
+            <Wrapper>
                 <ContainerInputs>
-                    <Label htmlFor="city">City</Label>
-                    <Input id="city" value={city} onChange={(e) => setCity(e.target.value)} type="text" />
-                    <Label htmlFor="breed">Breed</Label>
-                    <Input id="breed" value={breed} onChange={(e) => setBreed(e.target.value)} type="text" />
-                    <Label>Type</Label>
-                    <ul>
-                        <li>All</li>
-                        <li>Found</li>
-                        <li>Lost</li>
-                    </ul>
-
-                    <input type="file" onChange={(event: any) => setFile(event?.target?.files[0] as any)} />
-                    <Button onClick={handleUploadAnimal}>Add animal</Button>
+                    <ContainerInput>
+                        <Label htmlFor="city">City</Label>
+                        <Input id="city" value={city} onChange={(e) => setCity(e.target.value)} type="text" />
+                    </ContainerInput>
+                    <ContainerInput>
+                        <Label htmlFor="breed">Breed</Label>
+                        <Input id="breed" value={breed} onChange={(e) => setBreed(e.target.value)} type="text" />
+                    </ContainerInput>
+                    <ContainerInput>
+                        <Label>Type</Label>
+                        <select name="select">
+  <option value="value1">Value 1</option> 
+  <option value="value2" selected>Value 2</option>
+  <option value="value3">Value 3</option>
+</select>
+                    </ContainerInput>
                 </ContainerInputs>
-            </WrapperInfo>
+                <input type="file" onChange={(event: any) => setFile(event?.target?.files[0] as any)} />
+                <Button onClick={handleUploadAnimal}>Add animal</Button>
+
+            </Wrapper>
         </Container>
     )
 }
--- src/components/NewAnimal/styled.tsx
+++ src/components/NewAnimal/styled.tsx
@@ -1,11 +1,22 @@
 import styled from 'styled-components'
+const NAV_HEIGHT = "74px"
 
 export const Container = styled.div`
-
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #F1F2F5;
+    height: calc(100% - ${NAV_HEIGHT});
+    flex-direction: column;
 `
 
-export const WrapperInfo = styled.div`
-
+export const Wrapper = styled.div`
+    background: #FFF;
+    width: 540px;
+    max-width: 540px;
+    box-shadow: inset 0 -3px 0 rgba(84,94,111,.2);
+    padding: 30px;
+    border-radius: 15px;
 `
 
 export const Title = styled.h3`
@@ -17,14 +28,55 @@ export const SubMenu = styled.h4`
 `
 
 export const Label = styled.label`
+    margin: 0;
+`
+
+export const ContainerInput = styled.div`
+    display: flex;
+    flex-direction: column;
 `
 
 export const ContainerInputs = styled.div`
-
+    display: flex;
+    flex-direction: column;
 `
 
 export const Input = styled.input`
+    -webkit-appearance: none;
+    appearance: none;
+    border-radius: 5px;
+    box-shadow: inset 0 2px 0 rgba(84,94,111,.2);
+    font-family: geomanist,system;
+    font-weight: 400;
+    font-size: 16px;
+    color: rgba(84,94,111,.8);
+    padding: 9px 14px 5px;
+    background: rgba(84,94,111,.04);
+    border: none;
+    outline: none;
+    line-height: 28px;
+    margin: 10px 0px;
 `
 
 export const Button = styled.button`
+    border: none;
+    display: inline-block;
+    font-size: 16px;
+    font-family: geomanist,system;
+    font-weight: 400!important;
+    outline: none;
+    color: #fff;
+    border-radius: 46px;
+    cursor: pointer;
+    transition: transform .2s ease-in-out,background .2s ease-in-out;
+    background: #24b373;
+    box-shadow: inset 0 -3px 0 #1d915d;
+    line-height: 38px;
+    height: 42px;
+    padding: 0 22px;
+    width: auto;
+
+    &:focus {
+        outline: none;
+    }
 `
